d1eefa88da851e837768d166243073f331cc38b5
Яна <marmed0806@gmail.com>
2022-11-18 14:02:48 +0200

    my first commit with print

--- main.py
+++ main.py
@@ -1,28 +1,20 @@
-class Human:
-    def __init__(self, name, ves):
-        self.name = name
-        self.ves = ves
+from bs4 import BeautifulSoup
+import requests
+response = requests.get("https://minfin.com.ua/ua/currency/nbu/")
+if response.status_code == 200:
+     soup = BeautifulSoup(response.text, features="html.parser")
+     soup_list = soup.find("p", {"class": "sc-1x32wa2-9 glerpA"})
+     res1 = soup_list.__str__().split('glerpA">')
+     res2 = soup_list.__str__().split('<span')
+     dollar = res1[1][0:2]
+     dollar = dollar + "." + res2[0][len(res2[0])-2:len(res2[0])]
+     print(dollar)
 
-    def showInfo(self):
-        print("Name:", self.name, "ves:", self.ves)
-
-class Brain(Human):
-    def __init__(self, name, ves):
-        super().__init__(name, ves)
-
-class Leg(Human):
-    def __init__(self, name, ves):
-        super().__init__( name, ves)
-
-class Heart(Human):
-    def __init__(self, name, ves):
-        super().__init__(name, ves)
-
-myBrain = Brain("Brain", 1)
-myBrain.showInfo()
-
-myLeg = Leg("Leg", 5)
-myLeg.showInfo()
-
-myHeart = Heart("Heart", 0.2)
-myHeart.showInfo()
+class Converter:
+    def __init__(self,uahCount):
+        self.kursdollar = dollar
+        self.uahCount = uahCount
+    def convertToUah(self):
+        print(self.uahCount / float(self.kursdollar))
+obj = Converter(15000000000)
+obj.convertToUah()
